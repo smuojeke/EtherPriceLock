96e759715c2df38a3b81316fadaf142ebe62a3f8
Felix Watkins <felixwatkinz@gmail.com>
2017-09-06 10:45:10 +1200

    Create ExchangeRate.sol

---
+++ ExchangeRate.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.11;
+
+contract ExchangeRate {
+    uint public weiPerCent;
+    uint public lastBlock;
+
+    uint public centsPerEth;
+
+    OraclizeFacade oraclize;
+    ExchangeRateCallbackI receiver;
+
+    // TODO exchange rate should probably start "stale" and go stale after a time (for safety)
+    event UpdateExchangeRate(uint weiPerCent);
+    event FetchExchangeRate();
+
+    function ExchangeRate(address _servicesAddress) {
+        updateServices(_servicesAddress);
+    }
+
+    function updateServices(address _servicesAddress) {
+        ServicesI services = ServicesI(_servicesAddress);
+
+        oraclize = OraclizeFacade(services.oraclizeFacade());
+        oraclize.setCallback(this);
+
+        receiver = ExchangeRateCallbackI(services);
+    }
+
+    function receiveExchangeRate(uint _centsPerEth) {
+        //  wei/eth = 10**18
+        //
+        //  cents       wei         cents   eth
+        //  -------  /  ------  ==  ----- * ---
+        //  eth         eth         eth     wei
+        //
+        //  wei      cents      wei   eth       wei
+        //  ---   /  -----  ==  --- * ---    == ---
+        //  eth      eth        eth   cents     cents
+
+        uint _weiPerEth = 10**18;
+        uint _weiPerCent = _weiPerEth / _centsPerEth;
+
+        centsPerEth = _centsPerEth;
+        weiPerCent = _weiPerCent;
+        lastBlock = block.number;
+
+        UpdateExchangeRate(_weiPerCent);
+
+        receiver.receiveExchangeRate(_centsPerEth);
+    }
+
+    function initFetch() {
+        oraclize.query("URL", "json(https://api.etherscan.io/api?module=stats&action=ethprice).result.ethusd");
+        FetchExchangeRate();
+    }
+}
