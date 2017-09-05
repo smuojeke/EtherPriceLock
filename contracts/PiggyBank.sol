9ba51f10ab61260e59ca0e755857025818b523da
Matt <sifrawr@gmail.com>
2017-09-06 11:50:20 +1200

    added contracts folder, oraclizeAPI contract

---
+++ contracts/PiggyBank.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.4.13;
+import "oraclizeAPI.sol";
+
+contract PiggyBank is usingOraclize{
+
+  address public owner;
+  uint256 public priceThresholdInUsd;
+  uint256 public unlockTimestamp;
+  uint256 public bankBalance;
+  uint256 public ETHUSD;
+
+  function PiggyBank(uint256 _priceThresholdInUsd, uint256 _unlockTimestamp) {
+    owner = msg.sender;
+    priceThresholdInUsd = _priceThresholdInUsd;
+    unlockTimestamp = _unlockTimestamp;
+    isPriceLocked();
+  }
+
+  /** Modifiers */
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  modifier timeLocked {
+    require(isTimeLocked);
+    _;
+  }
+
+  modifier priceLocked {
+    require(isPriceLocked);
+    _;
+  }
+  /** END Modifiers */
+
+  function __callback(bytes32 myid, string result) {
+      if (msg.sender != oraclize_cbAddress()) throw;
+      ETHUSD = uint256(result);
+  }
+
+  function() payable {
+    deposit(msg.value);
+  }
+
+  function deposit(uint256 _amount) payable returns(bool) {
+    bankBalance += _amount;
+  }
+
+  function isTimeLocked() constant returns(bool) {
+    return (block.timestamp >= unlockTimestamp);
+  }
+
+  function updatePrice() {
+    oraclize_query("URL", "json(https://api.etherscan.io/api?module=stats&action=ethprice).result.ethusd");
+  }
+
+  function isPriceLocked() constant returns(bool) {
+    return (ETHUSD >= priceThresholdInUsd);
+  }
+
+  // function isLocked() constant returns(bool) {
+  //   return (isTimeLocked && isPriceLocked);
+  // }
+
+  function withdraw(uint256 _amount, address _to) priceLocked timeLocked onlyOwner {
+    _to.transfer(_amount);
+  }
+
+  function withdrawAll(address _to) priceLocked timeLocked onlyOwner {
+    _to.transfer(bankBalance);
+  }
+
+}
