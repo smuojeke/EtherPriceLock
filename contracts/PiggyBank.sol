49a53f16772b9ffdd7070af3e2b40921347d71ef
Matt <sifrawr@gmail.com>
2017-09-06 11:54:27 +1200

    added stringToUint helper

--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -33,9 +33,21 @@ contract PiggyBank is usingOraclize{
   }
   /** END Modifiers */
 
+  function stringToUint(string s) constant returns (uint result) {
+      bytes memory b = bytes(s);
+      uint i;
+      result = 0;
+      for (i = 0; i < b.length; i++) {
+          uint c = uint(b[i]);
+          if (c >= 48 && c <= 57) {
+              result = result * 10 + (c - 48);
+          }
+      }
+  }
+
   function __callback(bytes32 myid, string result) {
       if (msg.sender != oraclize_cbAddress()) throw;
-      ETHUSD = uint256(result);
+      ETHUSD = stringToUint(result);
   }
 
   function() payable {
