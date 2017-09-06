0b6f328539c539601d25830bedbfb32ff4b7dd65
Matt <sifrawr@gmail.com>
2017-09-06 15:42:59 +1200

    updated contract for multi-piggy banks

--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -8,30 +8,101 @@ contract PiggyBank is usingOraclize{
   uint256 public unlockTimestamp;
   uint256 public bankBalance;
   uint256 public ETHUSD;
+  mapping(address => Hodler) public hodlers;
 
-  function PiggyBank(uint256 _priceThresholdInUsd, uint256 _unlockTimestamp) {
-    owner = msg.sender;
-    priceThresholdInUsd = _priceThresholdInUsd;
-    unlockTimestamp = _unlockTimestamp;
-    isPriceLocked();
+  struct Piggy {
+    uint256 balance;
+    address owner;
+    uint256 priceThresholdInUsd;
+    uint256 unlockTimestamp;
+    string name;
+    bool exists;
   }
 
-  /** Modifiers */
+  struct Hodler {
+    string[] piggyList;
+    mapping(string => Piggy) piggies;
+    address owner;
+    bool exists;
+  }
+
   modifier onlyOwner {
     require(msg.sender == owner);
     _;
   }
 
-  modifier timeLocked {
-    require(isTimeLocked);
+  modifier onlyHodler {
+    require(hodlers[msg.sender].exists);
     _;
   }
 
-  modifier priceLocked {
-    require(isPriceLocked);
-    _;
+  function depositToPiggy(string _piggyName) payable onlyHodler {
+    var hodler = hodlers[msg.sender];
+    require(doesPiggyExist(_piggyName));
+    hodler.piggies[_piggyName].balance += msg.value;
   }
-  /** END Modifiers */
+
+  function withdrawFromPiggy(string _piggyName) onlyHodler {
+    var hodler = hodlers[msg.sender];
+    require(doesPiggyExist(_piggyName));
+    require(hodler.piggies[_piggyName].balance > 0);
+    require(!isPiggyLocked(_piggyName));
+    var transferAmount = hodler.piggies[_piggyName].balance;
+    hodler.piggies[_piggyName].balance = 0;
+    msg.sender.transfer(transferAmount);
+  }
+
+
+  function createPiggybank(uint256 _priceThresholdInUsd, uint256 _unlockTimestamp, string _piggyName) {
+    // Set up new hodler if not already existing
+    if (!hodlers[msg.sender].exists) {
+      hodlers[msg.sender].owner = msg.sender;
+      hodlers[msg.sender].exists = true;
+    }
+    require(hodlers[msg.sender].piggies[_piggyName].exists);
+    // Set up new piggy bank
+    var new_piggy = hodlers[msg.sender].piggies[_piggyName];
+    new_piggy.owner = msg.sender;
+    new_piggy.priceThresholdInUsd = _priceThresholdInUsd;
+    new_piggy.unlockTimestamp = _unlockTimestamp;
+    new_piggy.name = _piggyName;
+    new_piggy.exists = true;
+    // Save piggy bank name to list, and new piggy bank
+    hodlers[msg.sender].piggyList.push(_piggyName);
+  }
+
+  event LogPiggyBankName(string piggy_name);
+  function getPiggyNames() {
+    var hodler = hodlers[msg.sender];
+    if (hodler.exists) {
+      var numOfPiggies = hodler.piggyList.length;
+      for (var i = 0; i < numOfPiggies; i++) {
+        LogPiggyBankName(hodler.piggyList[i]);
+      }
+    }
+  }
+
+  function doesPiggyExist(string _piggyName) returns (bool) {
+    return (hodlers[msg.sender].piggies[_piggyName].exists);
+  }
+
+  function getPiggyBalance(string _piggyName) onlyHodler returns (uint256) {
+    if (!doesPiggyExist(_piggyName)) return 0;
+
+    return hodlers[msg.sender].piggies[_piggyName].balance;
+  }
+
+  function isPiggyLocked(string _piggyName) onlyHodler returns (bool) {
+    if (!doesPiggyExist(_piggyName)) return false;
+
+    var hodler = hodlers[msg.sender];
+    var piggy = hodler.piggies[_piggyName];
+    return ((block.timestamp >= piggy.unlockTimestamp) && (ETHUSD >= piggy.priceThresholdInUsd));
+  }
+
+  function PiggyBank() {
+    owner = msg.sender;
+  }
 
   function stringToUint(string s) constant returns (uint result) {
       bytes memory b = bytes(s);
@@ -46,40 +117,14 @@ contract PiggyBank is usingOraclize{
   }
 
   function __callback(bytes32 myid, string result) {
-      if (msg.sender != oraclize_cbAddress()) throw;
+    if (msg.sender != oraclize_cbAddress()) throw;
       ETHUSD = stringToUint(result);
   }
 
   function() payable {
-    deposit(msg.value);
-  }
-
-  function deposit(uint256 _amount) payable returns(bool) {
-    bankBalance += _amount;
-  }
-
-  function isTimeLocked() constant returns(bool) {
-    return (block.timestamp >= unlockTimestamp);
   }
 
   function updatePrice() {
     oraclize_query("URL", "json(https://api.etherscan.io/api?module=stats&action=ethprice).result.ethusd");
   }
-
-  function isPriceLocked() constant returns(bool) {
-    return (ETHUSD >= priceThresholdInUsd);
-  }
-
-  // function isLocked() constant returns(bool) {
-  //   return (isTimeLocked && isPriceLocked);
-  // }
-
-  function withdraw(uint256 _amount, address _to) priceLocked timeLocked onlyOwner {
-    _to.transfer(_amount);
-  }
-
-  function withdrawAll(address _to) priceLocked timeLocked onlyOwner {
-    _to.transfer(bankBalance);
-  }
-
 }
